3c3a687067aed9eb3dfd50bb61dc773ae7211372
Siva7 <siva.chaitanya0111@gmail.com>
2017-12-09 18:53:27 +0530

    Updated Class template with int Arrayconversion and chefandSequence

--- src/main/java/org/siva/ChefAndSequence.java
+++ src/main/java/org/siva/ChefAndSequence.java
@@ -3,18 +3,33 @@ package org.siva;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class modDistance {
+public class ChefAndSequence {
 
 	public static void main(String[] args) throws Exception, Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int noTS = Integer.parseInt(br.readLine());
+		int arrayLength,count,sum,product;
+		int[] intArray;
+		
 		for (int i=0;i<noTS;i++){
+			count=0;
+			arrayLength=Integer.parseInt(br.readLine());
+			intArray=stringArrayToIntArray(br.readLine().split(" "));
+			for(int j=0;j<arrayLength;j++){
+				sum=0;
+				product=1;
+				for(int k=j;k<arrayLength;k++){
+					sum+=intArray[k];
+					product*=intArray[k];
+					if (sum == product){count++;}
+					
+				}
+			}
 			
+			System.out.println(count);
 		}
 	}
-
-}
-
+	
 	public static int[] stringArrayToIntArray(String[] arg){
 		int stringArrayLength = arg.length;
 		int[] intArray = new int[stringArrayLength];
@@ -22,3 +37,6 @@ public class modDistance {
 		return intArray;
 		
 	}
+
+}
+
